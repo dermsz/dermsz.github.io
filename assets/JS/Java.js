15487fd833a6448804170465aa1595ae7bbdcefb
dermsz <diarmuidsmerriman@gmail.com>
2021-04-01 20:24:15 +0100

    js implementation

--- assets/JS/Java.js
+++ assets/JS/Java.js
@@ -3,7 +3,10 @@ var input = document.getElementById("listentry");
 var ul = document.getElementById("uselesslistentry")
 
 button.addEventListener("click", function() {
+    if (input.value.length > 0) {
     var li = document.createElement("li");
-    li.appendChild(document.createTextNode("testing"));
-    ul.appendChild(li)
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+    }
 })
