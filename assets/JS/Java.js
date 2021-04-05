9bf47eba0de0a49ecd16e3fc7a2224aa9a7b9392
dermsz <diarmuidsmerriman@gmail.com>
2021-04-05 16:22:23 +0100

    tidying java

--- assets/JS/Java.js
+++ assets/JS/Java.js
@@ -2,11 +2,22 @@ var button = document.getElementById("listbutton");
 var input = document.getElementById("listentry");
 var ul = document.getElementById("uselesslistentry")
 
-button.addEventListener("click", function() {
-    if (input.value.length > 0) {
+
+function inputlength() {
+    return input.value.length;
+}
+
+function createListElement(){
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
     input.value = "";
+}
+
+function addToList() {
+    if (inputlength() > 0) {
+        createListElement();
     }
-})
+}
+
+button.addEventListener("click", addToList)
